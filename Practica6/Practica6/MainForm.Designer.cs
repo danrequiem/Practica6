72c72307598f2731976efbd2106fad6be6eb05a7
danrequiem <DAN@iMac-de-Daniel.local>
2013-11-01 11:40:41 -0600

    Creacion del programa

---
+++ Practica6/Practica6/MainForm.Designer.cs
@@ -0,0 +1,271 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Alumnos
+ * Fecha: 31/10/2013
+ * Hora: 07:08 p.m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+namespace Practica6
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.radioButton1 = new System.Windows.Forms.RadioButton();
+			this.radioButton2 = new System.Windows.Forms.RadioButton();
+			this.radioButton3 = new System.Windows.Forms.RadioButton();
+			this.label4 = new System.Windows.Forms.Label();
+			this.Portugal = new System.Windows.Forms.CheckBox();
+			this.checkBox2 = new System.Windows.Forms.CheckBox();
+			this.ReinoUnido = new System.Windows.Forms.CheckBox();
+			this.checkBox4 = new System.Windows.Forms.CheckBox();
+			this.Macedonia = new System.Windows.Forms.CheckBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.label6 = new System.Windows.Forms.Label();
+			this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+			this.button1 = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(13, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 23);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Codigo";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(13, 40);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 23);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Nombre";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(63, 13);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 2;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(63, 40);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 3;
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(13, 67);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(150, 23);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "1. Capital de Mexico";
+			// 
+			// radioButton1
+			// 
+			this.radioButton1.Location = new System.Drawing.Point(13, 94);
+			this.radioButton1.Name = "radioButton1";
+			this.radioButton1.Size = new System.Drawing.Size(104, 24);
+			this.radioButton1.TabIndex = 5;
+			this.radioButton1.TabStop = true;
+			this.radioButton1.Text = "DF";
+			this.radioButton1.UseVisualStyleBackColor = true;
+			// 
+			// radioButton2
+			// 
+			this.radioButton2.Location = new System.Drawing.Point(73, 93);
+			this.radioButton2.Name = "radioButton2";
+			this.radioButton2.Size = new System.Drawing.Size(104, 24);
+			this.radioButton2.TabIndex = 6;
+			this.radioButton2.TabStop = true;
+			this.radioButton2.Text = "Guadalajara";
+			this.radioButton2.UseVisualStyleBackColor = true;
+			// 
+			// radioButton3
+			// 
+			this.radioButton3.Location = new System.Drawing.Point(183, 93);
+			this.radioButton3.Name = "radioButton3";
+			this.radioButton3.Size = new System.Drawing.Size(104, 24);
+			this.radioButton3.TabIndex = 7;
+			this.radioButton3.TabStop = true;
+			this.radioButton3.Text = "Monterrey";
+			this.radioButton3.UseVisualStyleBackColor = true;
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(13, 125);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(126, 23);
+			this.label4.TabIndex = 8;
+			this.label4.Text = "2. Paises de Europa";
+			// 
+			// Portugal
+			// 
+			this.Portugal.Location = new System.Drawing.Point(13, 152);
+			this.Portugal.Name = "Portugal";
+			this.Portugal.Size = new System.Drawing.Size(104, 24);
+			this.Portugal.TabIndex = 9;
+			this.Portugal.Text = "Portugal";
+			this.Portugal.UseVisualStyleBackColor = true;
+			// 
+			// checkBox2
+			// 
+			this.checkBox2.Location = new System.Drawing.Point(13, 183);
+			this.checkBox2.Name = "checkBox2";
+			this.checkBox2.Size = new System.Drawing.Size(104, 24);
+			this.checkBox2.TabIndex = 10;
+			this.checkBox2.Text = "Chile";
+			this.checkBox2.UseVisualStyleBackColor = true;
+			// 
+			// ReinoUnido
+			// 
+			this.ReinoUnido.Location = new System.Drawing.Point(13, 213);
+			this.ReinoUnido.Name = "ReinoUnido";
+			this.ReinoUnido.Size = new System.Drawing.Size(104, 24);
+			this.ReinoUnido.TabIndex = 11;
+			this.ReinoUnido.Text = "Reino Unido";
+			this.ReinoUnido.UseVisualStyleBackColor = true;
+			// 
+			// checkBox4
+			// 
+			this.checkBox4.Location = new System.Drawing.Point(13, 245);
+			this.checkBox4.Name = "checkBox4";
+			this.checkBox4.Size = new System.Drawing.Size(104, 24);
+			this.checkBox4.TabIndex = 12;
+			this.checkBox4.Text = "Australia";
+			this.checkBox4.UseVisualStyleBackColor = true;
+			// 
+			// Macedonia
+			// 
+			this.Macedonia.Location = new System.Drawing.Point(13, 276);
+			this.Macedonia.Name = "Macedonia";
+			this.Macedonia.Size = new System.Drawing.Size(104, 24);
+			this.Macedonia.TabIndex = 13;
+			this.Macedonia.Text = "Macedonia";
+			this.Macedonia.UseVisualStyleBackColor = true;
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(13, 307);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(286, 23);
+			this.label5.TabIndex = 14;
+			this.label5.Text = "3. Cantidad de edades de los estados que compone Mexico";
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(13, 334);
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(42, 20);
+			this.numericUpDown1.TabIndex = 15;
+			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(364, 67);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(250, 23);
+			this.label6.TabIndex = 16;
+			this.label6.Text = "4. Dia en la que termino la segunda guerra mundial";
+			// 
+			// monthCalendar1
+			// 
+			this.monthCalendar1.Location = new System.Drawing.Point(377, 93);
+			this.monthCalendar1.Name = "monthCalendar1";
+			this.monthCalendar1.TabIndex = 17;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(456, 307);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 18;
+			this.button1.Text = "Terminar";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(755, 389);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.monthCalendar1);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.Macedonia);
+			this.Controls.Add(this.checkBox4);
+			this.Controls.Add(this.ReinoUnido);
+			this.Controls.Add(this.checkBox2);
+			this.Controls.Add(this.Portugal);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.radioButton3);
+			this.Controls.Add(this.radioButton2);
+			this.Controls.Add(this.radioButton1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "Practica6";
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.MonthCalendar monthCalendar1;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.CheckBox Macedonia;
+		private System.Windows.Forms.CheckBox checkBox4;
+		private System.Windows.Forms.CheckBox ReinoUnido;
+		private System.Windows.Forms.CheckBox checkBox2;
+		private System.Windows.Forms.CheckBox Portugal;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.RadioButton radioButton3;
+		private System.Windows.Forms.RadioButton radioButton2;
+		private System.Windows.Forms.RadioButton radioButton1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ Practica6/Practica6/MainForm.cs
@@ -0,0 +1,107 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Alumnos
+ * Fecha: 31/10/2013
+ * Hora: 07:08 p.m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+ 
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Practica6
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public int aciertos = 0;
+		public string puntaje;
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		
+		void Button1Click(object sender, EventArgs e)
+		{
+			
+			pregunta1();
+			pregunta2();
+			pregunta3();
+			pregunta4();
+			puntajes();
+			MessageBox.Show("Codigo:" + this.textBox1.Text + "\n"+
+			                "Nombre:" + this.textBox2.Text + "\n"+
+			                "Aciertos:" +this.aciertos.ToString() + "\n"+
+			               	puntaje);
+			
+			
+		}
+		public void pregunta1(){
+			
+			if(this.radioButton1.Checked)
+			{
+				aciertos++;
+			}
+			
+		}
+		public void pregunta2(){
+			
+			if(this.ReinoUnido.Checked)
+			{
+				aciertos++;
+			}
+			if(this.Portugal.Checked)
+			{
+				aciertos++;
+			}
+			if(this.Macedonia.Checked)
+			{
+				aciertos++;
+			}
+		}
+		public void pregunta3(){
+			
+			if(this.numericUpDown1.Text == "31")
+			{
+				aciertos++;
+			}
+			
+		}
+		public void pregunta4(){
+			
+			
+			DateTime fecha = this.monthCalendar1.SelectionStart;
+			
+			if(fecha.ToShortDateString() == "07/05/1945")
+			{
+				aciertos++;
+			}
+		}
+		public void puntajes(){
+			
+			switch (aciertos){
+				
+				case 0: puntaje = "¡CONTESTA AUNQUE SEA UNA CAON!";
+					break;
+				case 1: puntaje = "¿PEÑA ERES TU?";
+					break;
+				case 2: puntaje = "¿PILLOS ERES TU?";
+					break;
+				case 3: puntaje = "ESFUERZATE MAS";
+					break;
+				case 4: puntaje = "PUEDES HACERLO MEJOR";
+					break;
+				case 5: puntaje = "BUEN PUNTAJE";
+					break;
+				case 6: puntaje = "ERES UN DIOS!";
+					break;
+			}
+			
+		}
+	}
+}
